0b6bcd1c4dd1c606c52490f0b33aac2fc33108e1
Pavinthan <pavinthan@outlook.com>
2017-11-09 09:45:03 +0530

    Pdf.php
    Temp directory not rightable fixed.

--- src/LaravelPdf/Pdf.php
+++ src/LaravelPdf/Pdf.php
@@ -24,7 +24,7 @@ class Pdf {
 		}
 
 		$this->mpdf = new Mpdf([
-		    'tempDir' => Config::get('pdf.custom_font_data'),
+		    'tempDir' => $this->getConfig('temp_dir'),
 		    $this->getConfig('mode'),              // mode - default ''
 		    $this->getConfig('format'),            // format - A4, for example, default ''
 		    $this->getConfig('default_font_size'), // font size - default 0
